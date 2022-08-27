0ea3a2418d63a9e5ff08d90ad89883bd07ef6ccc
Jack-Cannon <cannonjackj@gmail.com>
2022-08-26 22:24:23 -0500

    add Tailwind and appolo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useQuery } from "@apollo/client";
+import { GET_GRAPH } from "./queries";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { loading, error, data } = useQuery(GET_GRAPH, {
+    variables: { userName: "Jack-Cannon" },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  var processedData: any = [];
+
+  var semiFlat = data.user.contributionsCollection.contributionCalendar.weeks;
+  semiFlat.forEach((x: any) => {
+    processedData = [...processedData, x.contributionDays];
+  });
+
+  var contributions = processedData.flat();
+
+  var contributionValues: any = contributions
+    .map((x: any) => (x = { date: x.date, count: x.contributionCount }))
+    .filter((x: any) => x.count !== 0);
+
+  return <div>hey</div>;
 }
 
 export default App;
